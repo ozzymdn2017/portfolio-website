77aae529491350a3d13b8bd0daf5a237bbdf5f24
Bernini <oamedina1994@gmail.com>
2021-06-03 12:03:51 -0700

    Basic starting point with jit enabled and removal of default template components

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <Hero msg="Hello Vue 3 + Vite" />
+</template>
+
+<script setup>
+import Hero from './views/HeroPage.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
---
+++ src/views/HeroPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-center">{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+</script>
+
